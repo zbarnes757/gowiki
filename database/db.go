81f354e67540bddb22e5bc242fab042c67062241
Zac Barnes <zac.barnes89@gmail.com>
2018-07-30 13:57:09 -0500

    initialize db with envar

--- database/db.go
+++ database/db.go
@@ -3,6 +3,7 @@ package database
 import (
 	"fmt"
 	"gowiki/models"
+	"os"
 
 	"github.com/jinzhu/gorm"
 	// used to setup the connection
@@ -11,7 +12,7 @@ import (
 
 // SetupDB will migrate schemas and prepare the database and return a connection
 func SetupDB() *gorm.DB {
-	const addr = "postgresql://maxroach@localhost:26257/wiki?sslmode=disable"
+	addr := getDBAddress()
 	db, err := gorm.Open("postgres", addr)
 	if err != nil {
 		panic(fmt.Sprintf("failed to connect to database: %v", err))
@@ -21,4 +22,12 @@ func SetupDB() *gorm.DB {
 	db.AutoMigrate(&models.Page{})
 
 	return db
+}
+
+func getDBAddress() string {
+	if addr := os.Getenv("DATABASE_URL"); addr != "" {
+		return addr
+	}
+
+	return "postgresql://maxroach@localhost:26257/wiki?sslmode=disable"
 }
